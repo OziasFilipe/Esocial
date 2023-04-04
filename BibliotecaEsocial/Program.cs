74fb1ed368c845f3ed9a5e4d049e432d740a6c71
OziasFilipe <43150504+OziasFilipe@users.noreply.github.com>
2023-04-04 13:47:59 -0300

    Add files via upload

---
+++ BibliotecaEsocial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using BibliotecaEsocial.ClassesEsquemas;
+using BibliotecaEsocial.Eventos.S2190;
+using BibliotecaEsocial.Eventos.S2200;
+using BibliotecaEsocial.Eventos.S2230;
+using BibliotecaEsocial.Validacao;
+
+
+namespace BibliotecaEsocial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // S2200 s2200 = new S2200();
+           // s2200.tContato.emailPrinc = "ozias.pecanhaa@gmail.com";
+           // s2200.tContato.fonePrinc = "";
+           // s2200.S_2200();
+
+           S2230 s2230 = new S2230();
+           s2230.s2230();
+        }
+    }
+}
---
+++ BibliotecaEsocial/Validacao.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace BibliotecaEsocial
+
+{
+public class Validacao
+{
+    //private eSocialEvtTabEstab eSocialEvtTabEvtTabEstab = new eSocialEvtTabEstab();
+
+        public string GerarXml()
+        {
+           // XmlSerializer xml = new XmlSerializer(typeof(eSocial));
+           // xml.Serialize(Console.Out,esocial);
+            return "";  
+        }
+
+        public  string Validate(string nomeXml , string uriXml, string caminhoEsquema)
+        {
+            // Carrega o arquivo de esquema XSD
+            XmlSchemaSet schemaSet = new XmlSchemaSet();
+            schemaSet.Add(null, "C:\\Users\\Tecsoluti\\Desktop\\Esocial Oficial\\BibliotecaEsocial\\BibliotecaEsocial\\EsquemasXSD\\PacoteXSD\\evtAdmissao.xsd");
+            schemaSet.Compile();
+
+            // Cria o documento XML a partir do esquema
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Schemas.Add(schemaSet);
+            xmlDocument.LoadXml("<root><element1>value1</element1><element2>value2</element2></root>");
+
+            // Valida o documento XML
+            xmlDocument.Validate(null);
+
+            // Salva o arquivo XML válido
+            xmlDocument.Save("C:\\Users\\Tecsoluti\\Desktop\\Esocial Oficial\\BibliotecaEsocial\\BibliotecaEsocial\\XML\\evtAdmissao.xml");
+            
+            //Geração do XML para salvar como String 
+            // --> xmlDocument.InnerXml;
+
+            Console.WriteLine(xmlDocument.InnerXml);
+
+
+            // Lendo e validando XML contra XSD
+
+            LeituraXSD(schemaSet);
+
+            return "";
+        }
+
+        public void LeituraXSD(XmlSchemaSet schemaSet )
+        {
+
+            // Carrega o arquivo XML a ser validado
+            XmlReader reader = XmlReader.Create("C:\\Users\\Tecsoluti\\Desktop\\Esocial Oficial\\BibliotecaEsocial\\BibliotecaEsocial\\XML\\evtAdmissao.xml");
+            // Carrega o arquivo de esquema XSD
+
+            // Define as configurações de validação
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas.Add(schemaSet);
+
+            // Valida o arquivo XML
+            settings.ValidationEventHandler += ValidationEventHandler;
+            using (XmlReader validatingReader = XmlReader.Create(reader, settings))
+            {
+                while (validatingReader.Read())
+                {
+                    Console.WriteLine(validatingReader.Name);
+                }
+            }
+
+        }
+
+        public void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Error)
+            {
+                Console.WriteLine("Erro de validação: " + e.Message);
+            }
+            else
+            {
+                Console.WriteLine("Aviso de validação: " + e.Message);
+            }
+        }
+
+
+        
+        public void RegrasGerais()
+        {
+
+        }
+
+        public void S_1000()
+        {
+        }
+
+        public void s_1005()
+        {
+        }
+
+    }
+}
+
+
+
+
