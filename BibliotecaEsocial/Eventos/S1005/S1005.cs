915cc530ad1000e379972724965d8f967083c6d2
Renan de Aguiar <jasmyneleandro0@gmail.com>
2023-04-24 09:24:13 -0300

    fix: error item

---
+++ BibliotecaEsocial/Eventos/S1005/S1005.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using BibliotecaEsocial.Model.S1005;
+
+namespace BibliotecaEsocial.Eventos.S1005
+{
+    class S1005
+    {
+        private eSocialEvtTabEstab evtTabEstab = new eSocialEvtTabEstab();
+        private eSocialEvtTabEstabInfoEstab infoEstab = new eSocialEvtTabEstabInfoEstab();
+        private T_ideEmpregador t_ideEmpregador = new T_ideEmpregador();
+        private T_ideEvento_evtTab_inicial t_ideEvento_evtTab_inicial = new T_ideEvento_evtTab_inicial();
+        private eSocialEvtTabEstabInfoEstab item = new eSocialEvtTabEstabInfoEstab();
+
+
+
+        public string id = string.Empty;
+        public string nrInsc = string.Empty;
+        public sbyte tpInsc = 0;
+        public sbyte procEmi = 0;
+        public sbyte tpAmb = 0;
+        public string verProc = string.Empty;
+
+
+        public void s1005()
+        {
+            this.evtTabEstab.Id = this.id;
+            this.evtTabEstab.ideEmpregador = this.t_ideEmpregador;
+            this.evtTabEstab.infoEstab = this.infoEstab;
+            this.evtTabEstab.ideEvento = this.t_ideEvento_evtTab_inicial;
+
+            this.t_ideEmpregador.nrInsc = this.nrInsc;
+            this.t_ideEmpregador.tpInsc = this.tpInsc;
+
+            this.infoEstab.Item = this.item.Item; // com problema!! 
+
+            
+            
+            this.t_ideEvento_evtTab_inicial.procEmi = this.procEmi;
+            this.t_ideEvento_evtTab_inicial.tpAmb = this.tpAmb;
+            this.t_ideEvento_evtTab_inicial.verProc = this.verProc;
+
+           XmlSerializer xmlSerializer = new XmlSerializer(typeof(eSocialEvtTabEstab));
+           xmlSerializer.Serialize(Console.Out, evtTabEstab);
+           Console.ReadLine();
+        }
+
+    }
+}
---
+++ BibliotecaEsocial/Eventos/S1010/S1010.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaEsocial.Model.S1010;
+using BibliotecaEsocial.Model.S5002;
+using T_ideEmpregador = BibliotecaEsocial.Model.S1010.T_ideEmpregador;
+
+namespace BibliotecaEsocial.Eventos.S1010
+{
+    class S1010
+    {
+        private eSocialEvtTabRubrica evtTabRubrica = new eSocialEvtTabRubrica();
+        private T_ideEmpregador t_ideEmpregador = new T_ideEmpregador();
+        private T_ideEvento_evtTab t_ideEvento = new T_ideEvento_evtTab();
+        private eSocialEvtTabRubricaInfoRubrica evtInfoRubrica = new eSocialEvtTabRubricaInfoRubrica();
+       // private eSocialEvtTabRubricaInfoRubrica item = new eSocialEvtTabRubricaInfoRubrica();
+
+
+        public string id;
+        public sbyte tpInsc = 0;
+        public string nrInsc = string.Empty;
+        public object teste = new object();
+
+        public void s1010()
+        {
+            this.evtTabRubrica.Id = this.id;
+            this.evtTabRubrica.ideEvento = this.t_ideEvento;
+            this.evtTabRubrica.ideEmpregador = this.t_ideEmpregador;
+            this.evtTabRubrica.infoRubrica = this.evtInfoRubrica;
+
+            this.evtInfoRubrica = new eSocialEvtTabRubricaInfoRubrica();  // com problema!
+            this.evtInfoRubrica.Item = this.teste;
+            
+
+
+
+
+            this.t_ideEmpregador.tpInsc = this.tpInsc;
+            this.t_ideEmpregador.nrInsc = this.nrInsc;
+
+
+
+
+
+
+        }
+    }
+}
